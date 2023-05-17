ad0382ad4f71d18f6851277aa7523226b0849e54
Harrysauruss <mariusmey@outlook.com>
2023-05-17 14:55:02 +0200

    update

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args); 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello Worlds");
+
+app.MapGet("/check", async () => { 
+    try { 
+            var connectionString = "mongodb://root:example@mongo:27017/"; 
+            var mongoClient = new MongoClient(connectionString); 
+            var databaseNames = await mongoClient.ListDatabaseNames().ToListAsync(); 
+            var response = new { Databases = databaseNames }; 
+            return Results.Ok(response); 
+        } 
+        catch (Exception ex) { 
+            return Results.StatusCode(StatusCodes.Status500InternalServerError); 
+            } 
+        });
 
 app.Run();
